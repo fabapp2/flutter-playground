f454c56bbd529cc51bd0d4a9ab6d4b8ff3971fad
fabapp2 <fkrueger@pivotal.io>
2020-03-22 13:11:18 +0100

    feat:  replaced static star icon with stateful star icon

--- building_layouts/lib/main.dart
+++ building_layouts/lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(color, Icons.call, "CALL"),
+          // _buildButtonColumn(color, Icons.call, "CALL"),
+          FavoriteWidget(),
           _buildButtonColumn(color, Icons.near_me, "NEAR ME"),
           _buildButtonColumn(color, Icons.share, "SHARE")
         ],
@@ -89,7 +90,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
 
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+  int _favoriteCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          )
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text("$_favoriteCount"),
+          )
+        )
+      ],
+    );
+  }
+
+  _toggleFavorite() {
+    setState(()  {
+      if(_isFavorited) {
+        _favoriteCount--;
+        _isFavorited = false;
+      } else {
+        _favoriteCount++;
+        _isFavorited = true;
+      }
+    });
+  }
+}
 
 
 Widget titleSection = Container(
